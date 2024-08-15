101635a689f73f9d81275f2c66292dde9ee93cd5
viviarchives <wawaiceice@gmail.com>
2024-08-15 12:22:10 +0700

    Initial Commit

---
+++ cheappie/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  cheappie
+//
+//  Created by Warunya on 15/8/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Cheappie")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cheappie/cheappieApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheappieApp.swift
+//  cheappie
+//
+//  Created by Warunya on 15/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct cheappieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
