9da1ab366d4d7fa29f8784947372a8b8e7f90834
viviarchives <wawaiceice@gmail.com>
2024-08-20 11:44:21 +0700

    Add HomeView and MoreView and TabView

---
+++ cheappie/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  Home.swift
+//  cheappie
+//
+//  Created by Warunya on 20/8/2567 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+            .font(.Typography.title1Bold)
+    }
+}
+
+#Preview {
+    HomeView()
+}
